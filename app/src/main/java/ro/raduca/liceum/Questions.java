c502a8f697a9f2b40e328a4f99f059b19e142003
Simona Răduca <19simona.mihai@gmail.com>
2021-03-06 16:41:37 +0200

    Add question timer

--- app/src/main/java/ro/raduca/liceum/Questions.java
+++ app/src/main/java/ro/raduca/liceum/Questions.java
@@ -25,7 +25,7 @@ import ro.raduca.liceum.data.CategoryDatabase;
 public class Questions extends AppCompatActivity {
 
     public int visibility = 0;
-    public int currentCategoryIndex, questionListIndex = 0, numberQuestions, currentScore = 0;
+    public int currentCategoryIndex, questionListIndex = 0, questionIndex, currentScore = 0;
     boolean categoryFinished = true;
     int maxQuestions = 10;
     TextView ques;
@@ -37,10 +37,18 @@ public class Questions extends AppCompatActivity {
     String currentAnswer = null, currentQuestion, currentOptionA, currentOptionB, currentOptionC, currentOptionD;
     ArrayList<Integer> list = new ArrayList<>();
     Toast toast;
+    long timerDuration = 3000;
+    long timerStep = 1000;
+    CountDownTimer timer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        for (currentCategoryIndex = 1; currentCategoryIndex <= this.maxQuestions; currentCategoryIndex++) {
+            list.add(currentCategoryIndex);
+        }
+        Collections.shuffle(list);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
@@ -84,13 +92,103 @@ public class Questions extends AppCompatActivity {
         play_button = findViewById(R.id.play_button);
     }
 
+    public void addTimer () {
+        if (timer != null) {
+            timer.cancel();
+        }
+        timer = new CountDownTimer(timerDuration, timerStep) {
+            int progressBarValue = 100;
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progressBarValue = (int) (progressBarValue - 100*timerStep/timerDuration);
+                progressBar.setProgress(progressBarValue);
+            }
+
+            @Override
+            public void onFinish() {
+                final SharedPreferences shared = getSharedPreferences("Score", Context.MODE_PRIVATE);
+                toast.cancel();
+                SharedPreferences.Editor editor = shared.edit();
+                editor.putInt("Questions", questionIndex).apply();
+
+                if (get.equals("c1") && shared.getInt("Computer", 0) < currentScore)
+                    editor.putInt("Computer", currentScore * 10).apply();
+                else if (get.equals("c2") && shared.getInt("Sports", 0) < 1)
+                    editor.putInt("Sports", currentScore * 10).apply();
+                else if (get.equals("c3") && shared.getInt("Inventions", 0) < 1)
+                    editor.putInt("Inventions", currentScore * 10).apply();
+                else if (get.equals("c4") && shared.getInt("General", 0) < 1)
+                    editor.putInt("General", currentScore * 10).apply();
+                else if (get.equals("c5") && shared.getInt("Science", 0) < 1)
+                    editor.putInt("Science", currentScore * 10).apply();
+                else if (get.equals("c6") && shared.getInt("English", 0) < 1)
+                    editor.putInt("English", currentScore * 10).apply();
+                else if (get.equals("c7") && shared.getInt("Books", 0) < 1)
+                    editor.putInt("Books", currentScore * 10).apply();
+                else if (get.equals("c8") && shared.getInt("Maths", 0) < 1)
+                    editor.putInt("Maths", currentScore * 10).apply();
+                else if (get.equals("c9") && shared.getInt("Capitals", 0) < 1)
+                    editor.putInt("Capitals", currentScore * 10).apply();
+                else if (get.equals("c10") && shared.getInt("Currency", 0) < 1)
+                    editor.putInt("Currency", currentScore * 10).apply();
+
+                progressBar.setProgress(0);
+
+                if (questionIndex == maxQuestions-1) {
+                    Intent intent = new Intent(Questions.this, Result.class);
+                    intent.putExtra("correct", currentScore);
+                    intent.putExtra("attemp", questionIndex);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    displayNextQuestion();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    public void displayNextQuestion() {
+
+        questionIndex++;
+        switch (get) {
+            case "c1":
+                setUIQuestionElements("computer");
+                break;
+            case "c2":
+                setUIQuestionElements("sports");
+                break;
+            case "c3":
+                setUIQuestionElements("inventions");
+                break;
+            case "c4":
+                setUIQuestionElements("general");
+                break;
+            case "c5":
+                setUIQuestionElements("science");
+                break;
+            case "c6":
+                setUIQuestionElements("english");
+                break;
+            case "c7":
+                setUIQuestionElements("books");
+                break;
+            case "c8":
+                setUIQuestionElements("maths");
+                break;
+            case "c9":
+                setUIQuestionElements("capitals");
+                break;
+            case "c10":
+                setUIQuestionElements("currency");
+                break;
+        }
+    }
+
     public void onClick(View v) {
 
-        final SharedPreferences shared = getSharedPreferences("Score", Context.MODE_PRIVATE);
-        numberQuestions++;
-
         if (visibility == 0) {
-            // afisarea butoanelor care erau invizibile
             OptA.setVisibility(View.VISIBLE);
             OptB.setVisibility(View.VISIBLE);
             OptC.setVisibility(View.VISIBLE);
@@ -98,54 +196,6 @@ public class Questions extends AppCompatActivity {
             play_button.setVisibility(View.GONE);
             progressBar.setVisibility(View.VISIBLE);
             visibility = 1;
-
-            new CountDownTimer(200000, 1000) {
-                int progressBarValue = 100;
-
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    progressBarValue = progressBarValue - 5;
-                    progressBar.setProgress(progressBarValue);
-                }
-
-                @Override
-                public void onFinish() {
-                    toast.cancel();
-                    SharedPreferences.Editor editor = shared.edit();
-                    editor.putInt("Questions", numberQuestions).apply();
-
-                    if (get.equals("c1") && shared.getInt("Computer", 0) < currentScore)
-                        editor.putInt("Computer", currentScore * 10).apply();
-                    else if (get.equals("c2") && shared.getInt("Sports", 0) < 1)
-                        editor.putInt("Sports", currentScore * 10).apply();
-                    else if (get.equals("c3") && shared.getInt("Inventions", 0) < 1)
-                        editor.putInt("Inventions", currentScore * 10).apply();
-                    else if (get.equals("c4") && shared.getInt("General", 0) < 1)
-                        editor.putInt("General", currentScore * 10).apply();
-                    else if (get.equals("c5") && shared.getInt("Science", 0) < 1)
-                        editor.putInt("Science", currentScore * 10).apply();
-                    else if (get.equals("c6") && shared.getInt("English", 0) < 1)
-                        editor.putInt("English", currentScore * 10).apply();
-                    else if (get.equals("c7") && shared.getInt("Books", 0) < 1)
-                        editor.putInt("Books", currentScore * 10).apply();
-                    else if (get.equals("c8") && shared.getInt("Maths", 0) < 1)
-                        editor.putInt("Maths", currentScore * 10).apply();
-                    else if (get.equals("c9") && shared.getInt("Capitals", 0) < 1)
-                        editor.putInt("Capitals", currentScore * 10).apply();
-                    else if (get.equals("c10") && shared.getInt("Currency", 0) < 1)
-                        editor.putInt("Currency", currentScore * 10).apply();
-
-                    progressBar.setProgress(0);
-
-                    if (categoryFinished) {
-                        Intent intent = new Intent(Questions.this, Result.class);
-                        intent.putExtra("correct", currentScore);
-                        intent.putExtra("attemp", numberQuestions);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-            }.start();
         }
 
         if (currentAnswer != null) {
@@ -191,47 +241,11 @@ public class Questions extends AppCompatActivity {
             }
         }
 
-        switch (get) {
-            case "c1":
-                setUIQuestionElements("computer");
-                break;
-            case "c2":
-                setUIQuestionElements("sports");
-                break;
-            case "c3":
-                setUIQuestionElements("inventions");
-                break;
-            case "c4":
-                setUIQuestionElements("general");
-                break;
-            case "c5":
-                setUIQuestionElements("science");
-                break;
-            case "c6":
-                setUIQuestionElements("english");
-                break;
-            case "c7":
-                setUIQuestionElements("books");
-                break;
-            case "c8":
-                setUIQuestionElements("maths");
-                break;
-            case "c9":
-                setUIQuestionElements("capitals");
-                break;
-            case "c10":
-                setUIQuestionElements("currency");
-                break;
-        }
-
-
+        displayNextQuestion();
     }
 
     private void setUIQuestionElements(String databaseName) {
-        for (currentCategoryIndex = 1; currentCategoryIndex < this.maxQuestions; currentCategoryIndex++) {
-            list.add(currentCategoryIndex);
-        }
-        Collections.shuffle(list);
+        addTimer();
 
         currentQuestion = databases.get(databaseName).readQuestion(list.get(questionListIndex));
         currentOptionA = databases.get(databaseName).readOptionA(list.get(questionListIndex));
@@ -251,14 +265,12 @@ public class Questions extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         categoryFinished = false;
-        SharedPreferences sp = getSharedPreferences("Score", Context.MODE_PRIVATE);
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         categoryFinished = false;
-        SharedPreferences sp = getSharedPreferences("Score", Context.MODE_PRIVATE);
     }
 
     @Override
